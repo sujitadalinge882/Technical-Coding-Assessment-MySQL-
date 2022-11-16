2a8606fbb19364e1113ec2d229e1e14f49a0812a
sujit vijay adalinge <110627737+sujitadalinge882@users.noreply.github.com>
2022-11-17 00:44:41 +0530

    Add files via upload

---
+++ sql assignment.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS countries ( 
+COUNTRY_ID varchar(2),
+COUNTRY_NAME varchar(40)
+CHECK(COUNTRY_NAME IN('Italy','India','China')) ,
+REGION_ID decimal(10,0)
+);
+
+DESC countries;
